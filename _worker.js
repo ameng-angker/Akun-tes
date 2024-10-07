fca0060162d7b5290859048477616446193a9911
Gendar124 <161517312+Gendar124@users.noreply.github.com>
2024-10-07 21:40:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -608,8 +608,8 @@ function getVLESSConfig(userID, hostName) {
 	const vlessMain6 = `vless://${userID}\u0040zoomcares.zoom.us:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#CONFERENSI`
 	return 
 
-		const วเลสMain = ${userID}\u0040${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}`;
-		const วเลสSec = ${userID}\u0040${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}`;
+		const วเลสMain = 
+		const วเลสSec = 
 		return 
 ---------------------------------------------------------------
 ${วเลสMain}
