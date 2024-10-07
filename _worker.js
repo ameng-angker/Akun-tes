268ae867bbe9437009bd4c3deed8c91e2b6f2ed4
Gendar124 <161517312+Gendar124@users.noreply.github.com>
2024-10-07 17:37:06 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,6 +7,7 @@ import { connect } from 'cloudflare:sockets';
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
 let proxyIP = '172.232.238.169';
+let pingIP = 'www.bing.com';
 
 
 if (!isValidUUID(userID)) {
