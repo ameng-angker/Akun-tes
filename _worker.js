df7c887cfcde948c656803e580fd5c0a6514602c
Gendar124 <161517312+Gendar124@users.noreply.github.com>
2024-10-07 22:27:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -606,7 +606,7 @@ function getVLESSConfig(userID, hostName) {
 	const vlessMain3 = `vless://${userID}\u0040cdn.appsflyer.com:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#GAME_${hostName}`
 	const vlessMain4 = `vless://${userID}\u0040104.17.3.81:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#EDUKASI_${hostName}`
 	const vlessMain5 = `vless://${userID}\u0040Www.sushiroll.co.id:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#SUSHIROL_${hostName}`
-	const vlessMain6 = `vless://${userID}\u0040zoomcares.zoom.us:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#CONFERENSI_${hostName}`
+	const vlessMain6 = `vless://${userID}\u0040zoomcares.zoom.us:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}`
 	return '
 	
 ################################################################
