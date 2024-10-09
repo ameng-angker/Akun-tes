1a312aa10b66e5f3fbbbf9a705ab4a3c5ee51d76
Gendar124 <161517312+Gendar124@users.noreply.github.com>
2024-10-10 00:10:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-let proxyIP = '92.222.153.172';
+let proxyIP = '172.232.234.189';
 
 
 if (!isValidUUID(userID)) {
@@ -100,7 +100,7 @@ async function vlessOverWSHandler(request) {
 			const {
 				hasError,
 				message,
-				portRemote = 443,
+				portRemote = 587,
 				addressRemote = '',
 				rawDataIndex,
 				vlessVersion = new Uint8Array([0, 0]),
