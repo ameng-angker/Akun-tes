4c0e86143976affda2c49d01165d90384058261d
Gendar124 <161517312+Gendar124@users.noreply.github.com>
2024-10-10 00:04:31 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-let proxyIP = '45.60.186.91';
+let proxyIP = '92.222.153.172';
 
 
 if (!isValidUUID(userID)) {
