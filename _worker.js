a018491c1586432059641e3051f797a001e6b7f1
Gendar124 <161517312+Gendar124@users.noreply.github.com>
2024-10-11 22:09:46 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -475,6 +475,7 @@ function getVLESSConfig(userID, hostName) {
 	const vlessMain4 = `vless://${userID}\u0040104.17.3.81:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#EDUKASI`
 	const vlessMain5 = `vless://${userID}\u0040Www.sushiroll.co.id:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#SUSHIROLL`
 	const vlessMain6 = `vless://${userID}\u0040zoomcares.zoom.us:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#CONFERENSI`
+	const vlessMain7 = `vless://${userID}\u0040162.159.138.78:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#BYU`
 	return `
 ################################################################
 
@@ -524,9 +525,12 @@ ${vlessMain6}
 AKUN GAME
 ---------------------------------------------------------------
 ${vlessMain3}
-
 ---------------------------------------------------------------
 
+AKUN BYU
+---------------------------------------------------------------
+${vlessMain7}
+---------------------------------------------------------------
 
 
 ################################################################
